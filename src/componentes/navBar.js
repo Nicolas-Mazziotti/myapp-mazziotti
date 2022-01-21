6c2dc465429053c062d3f10b0cb72df611d91a03
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-01-21 14:00:30 -0300

    navbar

---
+++ src/componentes/navBar.js
@@ -0,0 +1,12 @@
+
+const navBar = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Productos</li>
+            <li>Nosotros</li>
+            <li>Contacto</li>
+        </ul>
+    )
+}
+export default navBar;
